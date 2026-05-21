719052fe8e3d66e54aaad5adbd8b098f6b551f7b
Jonathon Edwards <jpedwards8305@gmail.com>
2026-05-21 07:34:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
-from snowflake.snowpark.functions import col
 
 
 name_on_order = st.text_input('Name on Smoothie')
