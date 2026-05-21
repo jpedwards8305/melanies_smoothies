6f2a9bf5215b54cf64b6d00c6835bae0a6581aaa
Jonathon Edwards <jpedwards8305@gmail.com>
2026-05-21 07:08:47 -0600

    Reverted back to original

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-streamlit.title('My Parents New Healthy Diner')
+from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
