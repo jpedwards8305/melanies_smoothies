2e551877bdaa1518cd09d4146bdc570e8511dd54
Jonathon Edwards <jpedwards8305@gmail.com>
2026-05-25 08:41:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 # Import python packages
-pip install snowflake-snowpark-python
 import streamlit as st
 from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
