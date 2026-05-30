52272723cd37334a7fa06c9bb8f48c284a8e0e2d
Jonathon Edwards <jpedwards8305@gmail.com>
2026-05-30 09:32:53 -0600

    Import requests and pandas in streamlit_app.py
    Added imports for requests and pandas to the streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
+
+
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
